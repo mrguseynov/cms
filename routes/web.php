c0dbfbb6f20210bfbf63c57b96f2711bcf4052b1
mrguseynov <jegeyn@gmail.com>
2021-05-06 23:21:28 +0400

    Dashboard Index Page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dash\DashHomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['role:admin'])->prefix('root')->group( function () {
+    Route::get('/', [DashHomeController::class, 'index'])->name('homeAdmin');
+    //Route::resource('category', CategoryController::class);
+    //Route::resource('post', PostController::class);
+});
